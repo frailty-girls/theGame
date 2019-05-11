83b319f5b255eaabb9a262b2569e3eb11b091d95
Lulu <>
2019-05-11 16:15:19 +0800

    no message

---
+++ Assets/Scripts/textDisplayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textDisplayer : MonoBehaviour
+{
+   
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       GetComponent<TextMesh>().text = "uuu";
+    }
+}
